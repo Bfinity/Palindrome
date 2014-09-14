b51dc022606efeddd5e664d62ee543be2766f1e7
Brandy Foster <brandy.foster@detroitlabs.com>
2014-09-14 11:46:15 -0400

    forked repository, created scr and wrote comments for package.

---
+++ scr/com/sample/palindrome/Palindrome.java
@@ -0,0 +1,14 @@
+package com.sample.palindrome;
+
+/**
+ * Created by BFineRocks on 9/14/14.
+ */
+
+/**
+ * This program will determine if the word the user enters is a palindrome or not. It will print out a message with the
+ * answer. It will take user input, check if the input is valid, run the test for palindrome determination and print
+ * results.
+ */
+
+public class Palindrome {
+}
